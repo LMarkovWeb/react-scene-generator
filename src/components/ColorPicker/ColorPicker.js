b000b3ff74786910d563ea63591c695d92a8ed96
Leonid Markov <lmarkov.webmaster@ya.ru>
2021-05-29 14:06:43 +0300

    add width for component Twitter Picker

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -30,8 +30,21 @@ const ColorPicker = ({ title, isShow, onSkinColorSelect }) => {
       {isShowState && (
         <div className="ColorPicker__wrapper">
           <TwitterPicker
+            width={"276px"}
             color={initColor}
-            onChangeComplete={handleChangeComplete} colors={['#FF6900', '#FCB900', '#7BDCB5', '#00D084', '#8ED1FC', '#0693E3', '#ABB8C3', '#EB144C', '#F78DA7', '#9900EF']}
+            onChangeComplete={handleChangeComplete}
+            colors={[
+              "#FF6900",
+              "#FCB900",
+              "#7BDCB5",
+              "#00D084",
+              "#8ED1FC",
+              "#0693E3",
+              "#ABB8C3",
+              "#EB144C",
+              "#F78DA7",
+              "#9900EF",
+            ]}
           />
         </div>
       )}
