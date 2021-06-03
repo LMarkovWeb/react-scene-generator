6c1aa43fee78774fe14e6311cdc52653f69d2a14
ilyapasyuk <pasyuk.com@gmail.com>
2021-06-03 19:52:26 +0300

    create global state

--- src/components/Dating/Dating.js
+++ src/components/Dating/Dating.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React, {useContext, useState, useReducer} from "react";
 import "./style.scss";
 import Linear from "../Linear/Linear";
+import {StoreContext} from "../../context/storeContext";
+import {Head} from "../Head/Head";
 
 const dialogs = {
-  1: {
-    1: "Привет, я горячий сексхазбент",
-    2: "&#128513; &#128513; ахах) это кто? аха сексхазбент - это зачет)",
-    3: "У меня есть интересная книжка, хочешь почитать?",
-    4: "Что за книжка? Познавательная?",
-    5: "Конечно! Хочешь почитаю тебе на ночь?",
-    6: "Ну раз познавательная, то это всегда актуально",
-    7: "У меня, вообще, замечательная библиотека, могу как нибуть показать &#128521;",
-  },
-  2: {
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
-    6: "",
-  },
+    1: {
+        1: "Привет, я горячий сексхазбент",
+        2: "&#128513; &#128513; ахах) это кто? аха сексхазбент - это зачет)",
+        3: "У меня есть интересная книжка, хочешь почитать?",
+        4: "Что за книжка? Познавательная?",
+        5: "Конечно! Хочешь почитаю тебе на ночь?",
+        6: "Ну раз познавательная, то это всегда актуально",
+        7: "У меня, вообще, замечательная библиотека, могу как нибуть показать &#128521;",
+    },
+    2: {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: "",
+        6: "",
+    },
 };
 
 const Dating = () => {
-  return (
-    <div className="Dating">
-      <Linear />
-      <div className="Actor--left"></div>
-      <div className="Dialog"></div>
-      <div className="Actor--right"></div>
-    </div>
-  );
+    const {state, dispatch} = useContext(StoreContext)
+
+    return (
+        <div className="Dating">
+            <Linear/>
+
+            {state.head && <Head svgCode={state.head.preview}/>}
+
+            <div className="Actor--left"></div>
+            <div className="Dialog"></div>
+            <div className="Actor--right"></div>
+        </div>
+    );
 };
 
-export { Dating };
+export {Dating};
--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
 /**
  * React
  */
-import React, { useEffect, useState } from "react";
+import React, {useContext, useEffect, useReducer, useState} from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import {ACTION, INITIAL_STORE, Provider, StoreContext} from "./context/storeContext";
 
 /**
  * Components
  */
-import { List } from "./components/List/List";
-import { Head } from "./components/Head/Head";
-import { Legs } from "./components/Legs/Legs";
-import { Body } from "./components/Body/Body";
-import { SelectGender } from "./components/SelectGender/SelectGender";
-import { ColorPicker } from "./components/ColorPicker/ColorPicker";
-import {
-  arHeadsItemsGirl,
-  arHeadsItemsMan,
-  arBodiesItemsGirl,
-  arBodiesItemsMan,
-  arLegsItemsGirl,
-  arLegsItemsMan,
-} from "./data";
-import Nav from "./components/Nav";
+import {SceneGenerator} from "./components/SceneGenerator";
 import { Dating } from "./components/Dating/Dating";
 
 /**
@@ -36,94 +24,33 @@ import "./index.scss";
  * App
  */
 const App = () => {
-  // set default gender
-  let genderDefault = "man";
-  let genderFromLocSt = localStorage.getItem("genderDefault");
-  if (genderFromLocSt) {
-    genderDefault = genderFromLocSt;
+
+  const reducer = (currentState, payload) => {
+
+    switch (payload.action) {
+      case ACTION.UPDATE_HEAD:
+        return {
+          ...currentState,
+          head: payload.data
+        }
+    }
   }
 
-  // const label = [1, 2, 3, 4, 5];
-  // let _local = JSON.parse(localStorage.getItem("label") || "[]");
-  // _local.push(label);
-  // localStorage.setItem("label", JSON.stringify(_local));
-
-  const [head, setHead] = useState(false);
-  const [body, setBody] = useState(false);
-  const [legs, setLegs] = useState(false);
-  const [skinColor, setSkinColor] = useState("#FFDFC4");
-  const [gender, setGender] = useState(genderDefault);
-
-  useEffect(() => {
-    setHead(false);
-    setBody(false);
-    setLegs(false);
-  }, [gender]);
-
-  // debugger;
-
-  let currentListHead = gender === "woman" ? arHeadsItemsMan : arHeadsItemsGirl;
-  let currentListBody =
-    gender === "woman" ? arBodiesItemsMan : arBodiesItemsGirl;
-  let currentListLegs = gender === "woman" ? arLegsItemsMan : arLegsItemsGirl;
-
-  // ??? @see https://coderoad.ru/40256673/getElementById-%D0%B2-React
-  // const btn = document.getElementById("doNothing");
-  // console.log(btn);
+   const [state, dispatch] = useReducer(reducer, INITIAL_STORE)
 
   return (
+      <Provider value={{state, dispatch}}>
     <Router>
-      <div className="App">
         <Switch>
           <Route path="/dating">
             <Dating />
           </Route>
           <Route path="/">
-            <SelectGender gender={gender} onItemSelect={setGender} />
-
-            <aside className="App__aside--left">
-              <h1>
-                {gender === "man"
-                  ? "Создай свою идеальную девушку"
-                  : "Создай своего идеального парня"}
-              </h1>
-              <List
-                list={currentListHead}
-                title="Голова и прическа"
-                isShow
-                onItemSelect={setHead}
-                genderType={gender}
-              />
-              <ColorPicker
-                title="Цвет кожи"
-                isShow
-                onSkinColorSelect={setSkinColor}
-              />
-            </aside>
-            <div className="App__mannequin">
-              {head && <Head fillColor={skinColor} svgCode={head.preview} />}
-              {body && <Body fillColor={skinColor} svgCode={body.preview} />}
-              {legs && <Legs fillColor={skinColor} svgCode={legs.preview} />}
-            </div>
-            <aside className="App__aside--rigth">
-              <List
-                list={currentListBody}
-                title="Тело и верхняя одежда"
-                isShow
-                onItemSelect={setBody}
-              />
-              <List
-                list={currentListLegs}
-                title="Ноги"
-                isShow
-                onItemSelect={setLegs}
-              />
-              <Nav />
-            </aside>
+            <SceneGenerator/>
           </Route>
         </Switch>
-      </div>
     </Router>
+  </Provider>
   );
 };
 
