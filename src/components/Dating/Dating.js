c6f3433c1acdb35155a3ce301783635dc29ffcdc
Leonid Markov <lmarkov.webmaster@ya.ru>
2021-06-04 14:54:07 +0300

    all components + color

--- src/components/Dating/Dating.js
+++ src/components/Dating/Dating.js
@@ -1,43 +1,79 @@
-import React, {useContext, useState, useReducer} from "react";
-import "./style.scss";
+/**
+ * React
+ */
+import React, { useContext } from "react";
+/**
+ * Components
+ */
 import Linear from "../Linear/Linear";
-import {StoreContext} from "../../context/storeContext";
-import {Head} from "../Head/Head";
+import { Head } from "../Head/Head";
+import { Body } from "../Body/Body";
+import { Legs } from "../Legs/Legs";
+/**
+ * Services
+ */
+import "../../services/localstorage";
+/**
+ * Context
+ */
+import { StoreContext } from "../../context/storeContext";
+/**
+ * styles
+ */
+import "./style.scss";
+import { getLocSt } from "../../services/localstorage";
 
 const dialogs = {
-    1: {
-        1: "Привет, я горячий сексхазбент",
-        2: "&#128513; &#128513; ахах) это кто? аха сексхазбент - это зачет)",
-        3: "У меня есть интересная книжка, хочешь почитать?",
-        4: "Что за книжка? Познавательная?",
-        5: "Конечно! Хочешь почитаю тебе на ночь?",
-        6: "Ну раз познавательная, то это всегда актуально",
-        7: "У меня, вообще, замечательная библиотека, могу как нибуть показать &#128521;",
-    },
-    2: {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-        5: "",
-        6: "",
-    },
+  1: {
+    1: "Привет, я горячий сексхазбент",
+    2: "&#128513; &#128513; ахах) это кто? аха сексхазбент - это зачет)",
+    3: "У меня есть интересная книжка, хочешь почитать?",
+    4: "Что за книжка? Познавательная?",
+    5: "Конечно! Хочешь почитаю тебе на ночь?",
+    6: "Ну раз познавательная, то это всегда актуально",
+    7: "У меня, вообще, замечательная библиотека, могу как нибуть показать &#128521;",
+  },
+  2: {
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+  },
 };
 
 const Dating = () => {
-    const {state, dispatch} = useContext(StoreContext)
+  const { state, dispatch } = useContext(StoreContext);
+  //console.log("Файл Dating.js, state >> ", state);
 
-    return (
-        <div className="Dating">
-            <Linear/>
+  // @todo: необходимость условия для гендера?
+  // @todo: если сцена пуста, то сообщение. (при f5 стор пропадает)
+  // @todo: показ дефолтного партнера
 
-            {state.head && <Head svgCode={state.head.preview}/>}
-
-            <div className="Actor--left"></div>
-            <div className="Dialog"></div>
-            <div className="Actor--right"></div>
-        </div>
-    );
+  return (
+    <div className="Dating">
+      <Linear />
+      <div className="Actor--left">
+        {state.head && (
+          <Head fillColor={state.skinColor} svgCode={state.head.preview} />
+        )}
+        {state.body && (
+          <Body fillColor={state.skinColor} svgCode={state.body.preview} />
+        )}
+        {state.legs && (
+          <Legs fillColor={state.skinColor} svgCode={state.legs.preview} />
+        )}
+      </div>
+      <div className="Dialog"></div>
+      <div className="Actor--right">
+        {/* {console.log(
+          "component Dating, genderDefault = ",
+          getLocSt("genderDefault")
+        )} */}
+      </div>
+    </div>
+  );
 };
 
-export {Dating};
+export { Dating };
