02fffa274e9ef751223b47c6c04fd2392bde8cc7
Leonid Markov <lmarkov.webmaster@ya.ru>
2021-06-04 14:53:00 +0300

    styling

--- src/context/storeContext.js
+++ src/context/storeContext.js
@@ -3,22 +3,18 @@ import React from "react";
 export const ACTION = {
   UPDATE_HEAD: "UPDATE_HEAD",
   UPDATE_BODY: "UPDATE_BODY",
-  UPDATE_LEGS: "UPDATE_HEAD",
+  UPDATE_LEGS: "UPDATE_LEGS",
   UPDATE_COLOR: "UPDATE_COLOR",
-  UPDATE_COLOR: "UPDATE_GENDER",
 };
 
 export const INITIAL_STORE = {
   head: {
-    title: "",
     preview: "",
   },
   body: {
-    title: "",
     preview: "",
   },
   legs: {
-    title: "",
     preview: "",
   },
   skinColor: "",
